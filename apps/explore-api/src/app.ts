cbc9df2a0ba07bf5d93c0c8a737fc99db8f1d873
Nghia Nguyen <trongnghia.nguyen@outlook.com>
2022-02-18 21:16:17 +0700

    Support CORS

--- apps/explore-api/src/app.ts
+++ apps/explore-api/src/app.ts
@@ -1,5 +1,6 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
 import endpoints from './endpoints';
 import { errorHandler } from './middlewares/error-handler';
 import healthCheck from './middlewares/health-check';
@@ -10,6 +11,11 @@ const app = new Koa();
 if (process.env.APP_ENV === 'local') {
   app.use(httpLogger());
 }
+app.use(
+  cors({
+    maxAge: 3600,
+  })
+);
 app.use(healthCheck());
 app.use(bodyParser());
 app.use(errorHandler());
