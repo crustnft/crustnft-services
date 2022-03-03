bbb1c038a3dc419ceec400bcace015cd8d916259
Nghia Nguyen <trongnghia.nguyen@outlook.com>
2022-03-03 15:32:27 +0100

    Correct load envs

--- apps/explore-api/src/utils/load-env.ts
+++ apps/explore-api/src/utils/load-env.ts
@@ -1,9 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
-const APP_ENV = process.env.APP_ENV || 'local';
 
-export function loadDotEnv() {
-  dotenv.config({
-    path: path.resolve(process.cwd(), `.env.${APP_ENV}`),
-  });
-}
+const APP_ENV = process.env.APP_ENV || 'local';
+dotenv.config({
+  path: path.resolve(process.cwd(), `.env.${APP_ENV}`),
+});
