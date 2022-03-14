f03ca8dc3d99ea02b49cb4d35c1121e15b0f8198
Nghia Nguyen <trongnghia.nguyen@outlook.com>
2022-03-15 00:06:24 +0700

    Fix unit-test #17

--- apps/nft-generator-api/tests/integration/nft-collection.test.ts
+++ apps/nft-generator-api/tests/integration/nft-collection.test.ts
@@ -31,39 +31,52 @@ describe('test authentication', () => {
       .set('Accept', 'application/json');
 
     expect(response.status).toBe(400);
-    expect(response.body).toEqual({
-      error: {
-        details: [
-          { message: '"medias" is required', path: ['medias'] },
-          { message: '"account" is not allowed', path: ['account'] },
-        ],
-        message: 'BadRequestError',
-      },
-    });
   });
 
   test('should call method to create a new nft generator', async () => {
+    const payload = {
+      images: [
+        {
+          id: 'd1199842-d735-4b2f-9a78-f4f80e81df4f',
+          name: 'background',
+        },
+        {
+          id: '8383ffa8-51c8-4b75-9214-fcfe31d7127c',
+          name: 'head1',
+        },
+        {
+          id: 'giphy2.webp',
+          name: 'head',
+        },
+        {
+          id: 'giphy3.gif',
+          name: 'head',
+        },
+      ],
+      layers: [
+        {
+          name: 'Background',
+          imageIds: ['d1199842-d735-4b2f-9a78-f4f80e81df4f', 'giphy2.webp'],
+          id: 'IDLayer1',
+        },
+        {
+          imageIds: ['8383ffa8-51c8-4b75-9214-fcfe31d7127c', 'giphy3.gif'],
+          id: 'IDLayer2',
+          name: 'Costume',
+        },
+      ],
+      layerOrder: ['IDLayer1', 'IDLayer2'],
+      description: 'hello-toro333',
+      name: 'hello-toro3339',
+    };
+
     const response = await request
       .post('/api/v1/ntf-collections')
-      .send({
-        medias: [
-          {
-            category: 'background',
-            mediaId: '1',
-          },
-        ],
-      })
+      .send(payload)
       .set('Accept', 'application/json');
 
     expect(response.status).toBe(200);
-    expect(mockCreateNftGenerator).toBeCalledWith({
-      medias: [
-        {
-          category: 'background',
-          mediaId: '1',
-        },
-      ],
-    });
+    expect(mockCreateNftGenerator).toBeCalledWith(payload);
   });
 
   test('should return an nft-generator status ', async () => {
